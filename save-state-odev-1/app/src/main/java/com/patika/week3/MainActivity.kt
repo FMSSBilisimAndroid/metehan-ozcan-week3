9ea776b5a36a805adb3741f47061c3966f475896
Metehan Özcan <70481060+metehanozcan@users.noreply.github.com>
2022-09-11 01:06:39 +0300

    Created ActivityMain

---
+++ save-state-odev-1/app/src/main/java/com/patika/week3/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.patika.week3
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.patika.week3.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
