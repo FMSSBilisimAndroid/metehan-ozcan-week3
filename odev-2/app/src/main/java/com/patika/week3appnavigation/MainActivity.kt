ebf8b87bc0ce022d2932601a9c30412fd1bb2103
Metehan Özcan <70481060+metehanozcan@users.noreply.github.com>
2022-09-11 00:06:40 +0300

    Create MainActivity.kt
    Implemented Main

---
+++ odev-2/app/src/main/java/com/patika/week3appnavigation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.patika.week3appnavigation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+//MAIN ACTIVIY FOR LOGGED IN USER
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
